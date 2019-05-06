a847798556215c4d056bea72612781cca90c139b
rifkegribenes <rifkegribenes@gmail.com>
2019-05-05 22:37:14 -0700

    user system tests

--- tests/system/user_test.py
+++ tests/system/user_test.py
@@ -11,12 +11,24 @@ class UserTest(BaseTest):
 
                 self.assertEqual(r.status_code, 201)
                 self.assertIsNotNone(UserModel.find_by_username('test'))
-                self.assertDictEqual(d1={'message': 'User created successfully.'},
-                                     d2=json.loads(r.data))
+                self.assertDictEqual({'message': 'User created successfully.'}, json.loads(r.data))
 
     def test_register_and_login(self):
-        pass
+        with self.app() as c:
+            with self.app_context():
+                c.post('/register', data={'username': 'test', 'password': '1234'})
+                auth_response = c.post('/auth',
+                                      data=json.dumps({'username': 'test', 'password': '1234'}),
+                                      headers={'Content-Type': 'application/json'})
+
+                self.assertIn('access_token', json.loads(auth_response.data.keys()))
 
     def test_register_duplicate_user(self):
-        pass
+        with self.app() as c:
+            with self.app_context():
+                c.post('/register', data={'username': 'test', 'password': '1234'})
+                r = c.post('/register', data={'username': 'test', 'password': '1234'})
+
+                self.assertEqual(r.status_code, 400)
+                self.assertDictEqual({'message': "A user with username username already exists."}, json.loads(r.data))
 
